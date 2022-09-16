73a58dc9eb82f648680f49e0562804c03704afc7
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-09-16 23:46:16 +0800

    added act 1 exercise

---
+++ src/main/java/act1/PrintConsoleValue.java
@@ -0,0 +1,12 @@
+package act1;
+
+public class PrintConsoleValue {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------------------");
+        System.out.println("September 2022");
+        System.out.println("Java Refresher");
+        System.out.println("--------------------");
+    }
+}
