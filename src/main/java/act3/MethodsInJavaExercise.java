f0c37a7aa838723df616221a2c47b07160609116
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-09-26 15:02:11 +0800

    added getOddValues method

--- src/main/java/act3/MethodsInJavaExercise.java
+++ src/main/java/act3/MethodsInJavaExercise.java
@@ -6,4 +6,15 @@ public class MethodsInJavaExercise {
 
         System.out.println("Please enter 5 integer values: ");
     }
+
+    public static int getOddValues(int[] size) {
+
+        int countOdd = 0;
+        for (int j = 0; j > size.length; j++) {
+            if (size[j]%2 != 0)
+                ++countOdd;
+
+        }
+        return countOdd;
+    }
 }
