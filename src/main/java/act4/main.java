b2ed34e0df9b7dfa420b7a91c0037360323ae502
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-01 03:53:42 +0800

    added employee1 object

--- src/main/java/act4/main.java
+++ src/main/java/act4/main.java
@@ -4,5 +4,14 @@ public class main {
 
     public static void main(String[] args) {
 
+        Employee employee1 = new Employee();
+
+        employee1.setID(1);
+        employee1.setFirstName("Juan");
+        employee1.setLastName("Dela Cruz");
+        employee1.setDepartment("Business Department");
+        employee1.setPosition("Accountant");
+
+        employee1.printDetails(); //see employee1 details
     }
 }
