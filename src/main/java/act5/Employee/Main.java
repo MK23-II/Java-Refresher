ad63140e2e749d58ada41d7e4eca75deaaa72d87
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-02 04:46:39 +0800

    added manager object in Main class

--- src/main/java/act5/Employee/Main.java
+++ src/main/java/act5/Employee/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        Manager employee1 = new Manager(101234, "John Peter", "johnpeter@accenture.com", 20, 10);
+
+        //print details of employee1
+        employee1.printDetails();
     }
 }
