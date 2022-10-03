5e007efdd662a328efdb1010ebcd9fc1029fc3bc
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-03 14:28:02 +0800

    created vehicle object

--- src/main/java/act5/Vehicle/Parking.java
+++ src/main/java/act5/Vehicle/Parking.java
@@ -1,8 +1,30 @@
 package act5.Vehicle;
 
+import java.util.Scanner;
+
 public class Parking {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int doors, seats, wheels;
+
+        //create vehicle object
+        Vehicle vehicle1 = new Vehicle();
+        System.out.println("Number of doors: ");
+        doors = scan.nextInt();
+        System.out.println("Number of seats: ");
+        seats = scan.nextInt();
+        System.out.println("Number of wheels: ");
+        wheels = scan.nextInt();
+
+        //for spacing
+        System.out.println(" ");
+
         
+
+
+
     }
 }
