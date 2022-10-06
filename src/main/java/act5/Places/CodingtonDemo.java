05b9055561ccf60e0713b958be62340969c6aa0c
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-06 16:13:00 +0800

    added invoke comment at bottom

--- src/main/java/act5/Places/CodingtonDemo.java
+++ src/main/java/act5/Places/CodingtonDemo.java
@@ -1,15 +1,24 @@
 package act5.Places;
 
+import java.lang.reflect.Method;
+
 public class CodingtonDemo {
 
     public static void main(String[] args) {
 
+        //created object
         Theater ticket1 = new Theater();
 
+        //display area
         ticket1.showArea();
+
 
         ticket1.showEvents();
 
-        
+        //invoke showEvents()
+        /*Method m = ticket1.getClass().getDeclaredMethod("showEvents);
+        * System.out.println(m.invoke(ticket1));*/
+
+
     }
 }
