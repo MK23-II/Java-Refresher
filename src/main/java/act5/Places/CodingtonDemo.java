1fddf2dd24fb577d31aadbe08d409c4c7f55a518
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-06 16:09:28 +0800

    added demo class

---
+++ src/main/java/act5/Places/CodingtonDemo.java
@@ -0,0 +1,15 @@
+package act5.Places;
+
+public class CodingtonDemo {
+
+    public static void main(String[] args) {
+
+        Theater ticket1 = new Theater();
+
+        ticket1.showArea();
+
+        ticket1.showEvents();
+
+        
+    }
+}
