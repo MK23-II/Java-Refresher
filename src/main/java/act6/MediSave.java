a6c220e427d774aefcdef220a13b364920001b6b
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-17 00:35:50 +0800

    added method in MediSave account

--- src/main/java/act6/MediSave.java
+++ src/main/java/act6/MediSave.java
@@ -1,4 +1,13 @@
 package act6;
 
 public class MediSave extends Account {
+
+    public MediSave(String accountType, Contributor contributor) {
+        super(accountType, contributor);
+    }
+
+    @Override
+    public double getContributionRate() {
+        return 0.07;
+    }
 }
