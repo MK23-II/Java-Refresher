d04c40a4dcfa486c86c27a1a82b0b32308804557
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-17 00:37:22 +0800

    added method in special account

--- src/main/java/act6/Special.java
+++ src/main/java/act6/Special.java
@@ -1,4 +1,13 @@
 package act6;
 
 public class Special extends Account {
+
+    public Special(String accountType, Contributor contributor) {
+        super(accountType, contributor);
+    }
+
+    @Override
+    public double getContributionRate() {
+        return 0.07;
+    }
 }
