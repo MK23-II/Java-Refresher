8602cd6ccd70cd4196343a1a69110dcf888a98f9
MK23-II <95571996+MK23-II@users.noreply.github.com>
2022-10-17 00:34:12 +0800

    added method in ordinary account

--- src/main/java/act6/Ordinary.java
+++ src/main/java/act6/Ordinary.java
@@ -1,4 +1,13 @@
 package act6;
 
 public class Ordinary extends Account {
+
+    public Ordinary(String accountType, Contributor contributor) {
+        super(accountType, contributor);
+    }
+
+    @Override
+    public double getContributionRate() {
+        return 0.06;
+    }
 }
